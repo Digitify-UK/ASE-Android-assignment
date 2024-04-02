c6b6a7dbf858b22df1d68035d176eab512a12702
Asif Imran <asif.imran@digitify.com>
2024-04-02 15:31:46 +0500

    Practiced Inheritance

---
+++ app/src/main/java/com/example/myfirstapp/Inheritance.kt
@@ -0,0 +1,22 @@
+//package com.example.myfirstapp
+
+open class Employee(var name: String, var age: Int) {
+    open fun display(){
+        println(this.name)
+        println(this.age)
+    }
+
+}
+
+class Doctor(name: String, age: Int, var specialization :String ) : Employee(name,age) {
+    override fun display() {
+        super.display()
+        println(specialization)
+    }
+}
+
+
+fun main(){
+    val d = Doctor(name = "Asif", age = 25, specialization = "Operation ward")
+    d.display()
+}
