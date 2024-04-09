67e310528f2653e8cba8ac37d1a746b5af5c8d53
Asif Imran <asif.imran@digitify.com>
2024-04-09 15:56:14 +0500

    created generate lucky number app

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,15 +1,32 @@
 package com.example.myfirstapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        setContentView(R.layout.user_input_handling)
+        var edtUserName:EditText= findViewById(R.id.edtUserNameField)
+        var tvLuckyNumber: TextView = findViewById(R.id.tvLuckyNumber)
+        var tvuserName: TextView = findViewById(R.id.tvUserName)
+        val btnGenrateLuckyNumber= findViewById<Button>( R.id.btnGenerateLuckyNumber)
+        btnGenrateLuckyNumber.setOnClickListener{
+            var randomLuckyNumber= genrateRandomLuckyNumber()
+            tvLuckyNumber.setText("${randomLuckyNumber}")
+            tvuserName.setText("Hey! "+edtUserName.text)
+        }
 
-        setContentView(R.layout.text_field_practicing)
+    }
 
+    fun genrateRandomLuckyNumber():Int{
+        val randomNumber = Random.nextInt(1000)
+        return randomNumber
     }
 }
